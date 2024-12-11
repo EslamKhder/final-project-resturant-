d0e49a673f470ae67e7a76b47454108c757e5739
Eslam.khder <eslamkhder11@gmail.com>
2024-12-12 00:21:44 +0200

    final project

--- sqlfiles/1.Categories.sql
+++ sqlfiles/1.Categories.sql
@@ -1,7 +1,7 @@
 DELETE FROM CATEGORY;
 INSERT INTO CATEGORY (LOGO_PATH, NAME, FLAG) VALUES ('fa fa-shopping-cart', 'ALL', 'Popular');
 INSERT INTO CATEGORY (LOGO_PATH, NAME, FLAG) VALUES ('fa fa-hamburger', 'Foods', 'Special');
-INSERT INTO CATEGORY (LOGO_PATH, NAME, FLAG) VALUES ('fa fa-utensils', 'Dinner', 'Special');
+INSERT INTO CATEGORY (LOGO_PATH, NAME, FLAG) VALUES ('fa fa-pizza-slice', 'Fast Food', 'Special');
 INSERT INTO CATEGORY (LOGO_PATH, NAME, FLAG) VALUES ('fa fa-utensils', 'Other Foods', 'Lovely');
 INSERT INTO CATEGORY (LOGO_PATH, NAME, FLAG) VALUES ('fa fa-ice-cream', 'Cold Drinks', 'Lovely');
 INSERT INTO CATEGORY (LOGO_PATH, NAME, FLAG) VALUES ('fa fa-coffee', 'Hot Drinks', 'Lovely');
